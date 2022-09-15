c383910ce07ca180559fdd88735eef6f0c98352f
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-09-15 13:50:15 -0700

    Hypothetically this works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDrive.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="Mecanum Drive Example", group="Iterative Opmode")
+@TeleOp(name="Mecanum Drive", group="Iterative Opmode")
 
 public class MechDrive extends OpMode {
 
