2d8bfd3711ffc7dd73ce74434ffa7240a1541724
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-09-22 13:28:14 -0700

    a

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.lang.annotation.Annotation;
+
+@Autonomous(name="TestAuto", group="Linear Opmode")
+class TestAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        while(opModeIsActive()) {
+            telemetry.addData("Status", "Initialized");
+            telemetry.update();
+
+            waitForStart();
+
+        }
+    }
+}
