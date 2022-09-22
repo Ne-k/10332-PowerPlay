60c0c0847c12504c51e211d0bb03888db974289e
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-09-21 21:39:28 -0700

    asd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDrive.java
@@ -1,57 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
-@TeleOp
+@TeleOp(name="Mecanum Drive", group="Iterative Opmode")
 
-public class MechDrive extends OpMode {
-
-    private DcMotor front_left;
-    private DcMotor front_right;
-    private DcMotor back_left;
-    private DcMotor back_right;
+public class MechDrive extends LinearOpMode {
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
+        DcMotor front_left = hardwareMap.dcMotor.get(Constants.Motors.front_left);
+        DcMotor front_right = hardwareMap.dcMotor.get(Constants.Motors.front_right);
+        DcMotor back_left = hardwareMap.dcMotor.get(Constants.Motors.back_left);
+        DcMotor back_right = hardwareMap.dcMotor.get(Constants.Motors.back_right);
 
-        front_left   = hardwareMap.get(DcMotor.class, Constants.Motors.front_left);
-        front_right  = hardwareMap.get(DcMotor.class, Constants.Motors.front_right);
-        back_left    = hardwareMap.get(DcMotor.class, Constants.Motors.back_left);
-        back_right   = hardwareMap.get(DcMotor.class, Constants.Motors.back_right);
-    }
+        waitForStart();
 
-    @Override
-    public void loop() {
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
 
-        double drive  = gamepad1.left_stick_y;
-        double strafe = gamepad1.left_stick_x;
-        double twist  = gamepad1.right_stick_x;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1.0);
 
+            double front_left_power = (y + x + rx) / denominator;
+            double back_left_power = (y - x + rx) / denominator;
+            double front_right_power = (y - x - rx) / denominator;
+            double back_right_power = (y + x - rx) / denominator;
 
-        double[] speeds = {
-                (drive + strafe + twist),
-                (drive - strafe - twist),
-                (drive - strafe + twist),
-                (drive + strafe - twist)
-        };
-
-
-        double max = Math.abs(speeds[0]);
-        for (double speed : speeds) {
-            if (max < Math.abs(speed)) max = Math.abs(speed);
+            front_left.setPower(front_left_power);
+            back_left.setPower(back_left_power);
+            front_right.setPower(front_right_power);
+            back_right.setPower(back_right_power);
         }
-
-
-        if (max > 1) {
-            for (int i = 0; i < speeds.length; i++) speeds[i] /= max;
-        }
-
-        front_left.setPower(speeds[0]);
-        front_right.setPower(speeds[1]);
-        back_left.setPower(speeds[2]);
-        back_right.setPower(speeds[3]);
     }
 }
