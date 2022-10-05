a180cf234a4bca1932126f2480207f1cf93df049
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-10-05 14:45:55 -0700

    sd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arcadeDrive.java
@@ -5,16 +5,31 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
 @TeleOp(name="arcodeDrive", group="Linear Opmode")
 public class arcadeDrive extends LinearOpMode
 {
+
     DcMotor leftMotor, rightMotor;
     float   leftPower, rightPower, turnLeft, turnRight;
+    OpenCvCamera webcam;
 
-    // called when init button is  pressed.
     @Override
     public void runOpMode() throws InterruptedException
     {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Im watching you"), cameraMonitorViewId);
+
         leftMotor = hardwareMap.dcMotor.get(Constants.Motors.left);
         rightMotor = hardwareMap.dcMotor.get(Constants.Motors.right);
 
