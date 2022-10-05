1a1a4dd5f52dc455af34035799d06cd903e4c0d8
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-10-05 15:01:49 -0700

    sd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arcadeDrive.java
@@ -3,18 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
+import org.firstinspires.ftc.teamcode.pipelines.pipeline;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.openftc.easyopencv.OpenCvWebcam;
 
 @TeleOp(name="arcodeDrive", group="Linear Opmode")
 public class arcadeDrive extends LinearOpMode
@@ -30,6 +24,7 @@ public class arcadeDrive extends LinearOpMode
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Im watching you"), cameraMonitorViewId);
 
+        webcam.setPipeline(new pipeline());
         leftMotor = hardwareMap.dcMotor.get(Constants.Motors.left);
         rightMotor = hardwareMap.dcMotor.get(Constants.Motors.right);
 
@@ -41,11 +36,42 @@ public class arcadeDrive extends LinearOpMode
         // wait for start button.
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        waitForStart();
 
-        while (opModeIsActive())
-        {
-            leftPower =  gamepad1.left_stick_y;
+        waitForStart();
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
+                 * supports streaming from the webcam in the uncompressed YUV image format. This means
+                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
+                 *
+                 * Also, we specify the rotation that the webcam is used in. This is so that the image
+                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
+                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+                 * away from the user.
+                 */
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                // An error occurred. This is generally fatal and there is nothing
+                // that can be done
+            }
+        });
+
+        while (opModeIsActive()) {
+
+
+
+                leftPower =  gamepad1.left_stick_y;
             rightPower = gamepad1.left_stick_y;
 
             turnLeft = gamepad1.right_stick_x * -1;
@@ -62,6 +88,13 @@ public class arcadeDrive extends LinearOpMode
             leftMotor.setPower(turnLeft);
             rightMotor.setPower(turnRight);
 
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            telemetry.update();
             /*
              telemetry.addData("Mode", "running");
             telemetry.addData("stick", "  y=" + yValue + "  x=" + xValue);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/colorPipeline.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.pipelines;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class colorPipeline extends OpenCvPipeline{
+
+    @Override
+    public Mat processFrame(Mat input) {
+
+        return null;
+    }
+}
