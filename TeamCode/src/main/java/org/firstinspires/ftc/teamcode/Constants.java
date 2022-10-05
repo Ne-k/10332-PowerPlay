f1235a9592cf6dab9599a7873035b3ba0392d2aa
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-10-05 13:52:52 -0700

    sd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public class Constants {
     public static class Motors {
         public static String left = "left";
